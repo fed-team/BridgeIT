8eadc27696fb588260926b0a837e5a5a8a4cc7e8
Damian Orzeł <damian.orzel1995@gmail.com>
2019-07-01 10:46:13 +0200

    Clear Project | Added dedicated scss file for colors

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>BridgeIT</h1>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+@import url('https://fonts.googleapis.com/css?family=Poppins:400,600|Roboto&display=swap');
+@import '@/assets/style/colors.scss';
+body, *, html {
+  color: $grey-6;
+  font-family: 'Poppins', sans-serif;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+
+h1 {
+  text-align: center;
 }
 </style>
--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -10,7 +10,7 @@
 
 module.exports = (on, config) => {
   // on('file:preprocessor', webpack({
-  //  webpackOptions: require('@vue/cli-service/webpack.config'),
+  //  Options: require('@vue/cli-service/webpack.config'),
   //  watchOptions: {}
   // }))
 
