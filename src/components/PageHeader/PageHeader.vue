2ebb804a4243a486430f207eba367f63b15a8193
Damian Orzeł <dam.orzel@gmail.com>
2019-07-05 14:43:03 +0200

    Change colors.scss path in PageHeader.vue

--- src/components/PageHeader/PageHeader.vue
+++ src/components/PageHeader/PageHeader.vue
@@ -14,7 +14,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-  @import "@/assets/style/colors.scss";
+  @import "@/assets/style/variables/colors.scss";
   .page-header {
     display: flex;
     flex-direction: column;
